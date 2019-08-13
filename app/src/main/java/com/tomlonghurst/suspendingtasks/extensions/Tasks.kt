a6b2379bb7281544173720b366d6a861a49cebcc
DESKTOP-7GL3TC3\thomh <thomhurst@gmail.com>
2019-08-13 19:04:19 +0100

    Tasks.await to catch exceptions

--- app/src/main/java/com/tomlonghurst/suspendingtasks/extensions/Tasks.kt
+++ app/src/main/java/com/tomlonghurst/suspendingtasks/extensions/Tasks.kt
@@ -1,6 +1,7 @@
 package com.tomlonghurst.suspendingtasks.extensions
 
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import com.tomlonghurst.suspendingtasks.models.CompletedTask
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -14,6 +15,8 @@ suspend fun <T> Task<T>.await() = suspendCoroutine<CompletedTask<T?>> { continua
         addOnCanceledListener {
             continuation.resume(CompletedTask(task = null))
         }
+
+        Tasks.await(this)
     } catch (e: Exception) {
         continuation.resume(CompletedTask(e))
     }
